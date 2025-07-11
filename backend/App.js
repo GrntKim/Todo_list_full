741795aacff95b4f2946aad6e582b3d7afe86a33
GrntKim <grantjun0224@gmail.com>
2025-07-11 15:46:59 +0900

    feat: implement basic backend APIs and add todo creation feature in frontend

---
+++ backend/App.js
@@ -0,0 +1,44 @@
+import express from 'express';
+const app = express();
+app.use(express.json);
+
+let todos = [];
+
+// view todos
+app.get('api/todos', (req, res) => {
+    res.json(todos);
+});
+
+// add todos
+app.post('api/todos', (req, res) => {
+    const { content } = req.body;
+    const newTodo = {
+        id: Date.now(),
+        content,
+        createdAt: new Date().toISOString(),
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+// modify(status, content) todo
+app.patch('/api/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const { completed } = req.body;
+    const todo = todos.find(t => t.id == id);
+    if (todo) {
+        todo.completed = completed;
+        res.json(todo);
+    } else {
+        res.status(404).json({ error: 'Not found'});
+    }
+});
+
+// delete todo
+app.delete('/api/todos/:id', (req, res) => {
+    todos = todos.filter(t => t.id == req.params.id);
+    res.status(204).end();
+});
+
+app.listen(5000, () => console.log('Server started'));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,43 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleAdd = () => {
+    if (input.trim() === '') return;
+    setTodos([...todos, { id: Date.now(), content: input }]);
+    setInput('');
+  };
+
   return (
     <div>
       <h1>TODO LIST</h1>
       <div className='container'>
-        <input type='text' placeholder="Todo.." />
-        <button>Add</button>
+        <input type='text' 
+               placeholder="Input here.."
+               value={input}
+               onChange={handleInputChange}
+        />
+        <button onClick={handleAdd}>Add</button>
         <button>Clear</button>
         <button>Clear ALL</button>
       </div>
-      <div className='task-list'>
-        <ul>
-          <li>task1</li>
-          <li>task2</li>
-          <li>task3</li>
+      <div className='task-list-container'>
+        <ul className='task-list'>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              {todo.content}
+              <button>finished</button>
+              <button>delete</button>
+              <button>update</button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
